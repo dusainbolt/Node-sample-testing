832b628bafabf613e7bbb5dd573b7cfa90db36a7
le.huy.du <SUN-ASTERISK\le.huy.du@b122952-pc.sun-asterisk.com>
2022-02-28 23:11:39 +0700

    fix

--- signMessage.js
+++ signMessage.js
@@ -1,26 +1,16 @@
 const Web3 = require("web3");
 const ethers = require("ethers");
+const crypto = require("crypto-js");
 
-const getMessageHash = (arrayType = [], arrayData = []) => {
-  return ethers.utils.solidityKeccak256(arrayType, arrayData);
-};
+const BN = ethers.BigNumber;
 
 const _ = {
   RPC_POLYGON: "https://rpc-mumbai.maticvigil.com",
   CONTRACT_ADDRESS_POLYGON: "0xE247a62298fdFf6F89a688a15aD9F108Ae3EeBD5",
-  buyerAddress: "0x83eF7DEA9c0eD0CadE9Aed85702540CF5254c095",
-  tokenId: "10022",
-  metadata: "12321321321312321321",
   addressAdmin: "0x4001aB01ebf313c90b92f0cd7e7A1371db6ff5Fa",
   privateAddressAdmin:
     "997183d0aa6b358a4a9db62495dc38d68cce60a9c1601af56b8a75fd5e3915bd",
-  tokenIds: [18, 19, 20],
-  tokenIdExist: 11,
-  addressBuyerList: [
-    "0x83eF7DEA9c0eD0CadE9Aed85702540CF5254c095",
-    "0x0b7ccB468fc4422a2F32a481cA88793023a6Ed92",
-    "0x83eF7DEA9c0eD0CadE9Aed85702540CF5254c095",
-  ],
+  APP_KEY: "997183d0aa6b358a4a9db",
 };
 
 const web3Polygon = new Web3(_.RPC_POLYGON);
@@ -29,10 +19,17 @@ const web3Polygon = new Web3(_.RPC_POLYGON);
 
 // }
 
-const messageHash = ethers.utils.solidityKeccak256(
-  ["string", "string"],
-  ["Metareum Signature", "1645431377"]
-);
+// const messageHash = ethers.utils.solidityKeccak256(
+//   ["string", "string"],
+//   ["Metareum Signature", "1645431377"]
+// );
+
+const msgObj = {
+  originalMessage: "Metareum Signature",
+  dateTime: BN.from(Date.now()).div(1000).toString(),
+};
+
+const messageHash = crypto.AES.encrypt(JSON.stringify(msgObj), _.APP_KEY).toString();
 
 const signature = web3Polygon.eth.accounts.sign(
   messageHash,
